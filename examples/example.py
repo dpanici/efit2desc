6ae8a586fc52a33d93cddce78bf5197d6503e3f4
Dario Panici <dpanici@princeton.edu>
2025-08-26 19:29:55 -0700

    updated example

--- examples/example.py
+++ examples/example.py
@@ -1,8 +1,14 @@
 import sys
+
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '../efit2desc')
+sys.path.insert(1, "../efit2desc")  # insert path ti source so can import things
+
+import numpy as np
+
+print(np.__version__)
+print(np.__file__)
+print(sys.path)
 
 from efit2desc import convert_EFIT_to_DESC
 
-eq, efit = convert_EFIT_to_DESC("g200245.02200",
-                                L=16, M=16, psiN_cutoff=0.98)
+eq, efit = convert_EFIT_to_DESC("g200245.02200", L=16, M=16, psiN_cutoff=0.98)
